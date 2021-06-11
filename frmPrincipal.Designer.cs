8186efaa2b93aad61649dfb2d31b865176b02274
Vladimir Suarez <suarez.vladimir@gmail.com>
2021-06-11 00:44:14 -0600

    Add project files.

---
+++ frmPrincipal.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace ejemploDivision
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.txtDividendo = new System.Windows.Forms.TextBox();
+            this.txtDivisor = new System.Windows.Forms.TextBox();
+            this.lblDividendo = new System.Windows.Forms.Label();
+            this.lblDivisor = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtDividendo
+            // 
+            this.txtDividendo.Location = new System.Drawing.Point(12, 36);
+            this.txtDividendo.Name = "txtDividendo";
+            this.txtDividendo.Size = new System.Drawing.Size(100, 20);
+            this.txtDividendo.TabIndex = 2;
+            // 
+            // txtDivisor
+            // 
+            this.txtDivisor.AcceptsReturn = true;
+            this.txtDivisor.Location = new System.Drawing.Point(12, 84);
+            this.txtDivisor.Name = "txtDivisor";
+            this.txtDivisor.Size = new System.Drawing.Size(100, 20);
+            this.txtDivisor.TabIndex = 4;
+            // 
+            // lblDividendo
+            // 
+            this.lblDividendo.AutoSize = true;
+            this.lblDividendo.Location = new System.Drawing.Point(12, 20);
+            this.lblDividendo.Name = "lblDividendo";
+            this.lblDividendo.Size = new System.Drawing.Size(102, 13);
+            this.lblDividendo.TabIndex = 1;
+            this.lblDividendo.Text = "Ingrese el dividendo";
+            // 
+            // lblDivisor
+            // 
+            this.lblDivisor.AutoSize = true;
+            this.lblDivisor.Location = new System.Drawing.Point(12, 68);
+            this.lblDivisor.Name = "lblDivisor";
+            this.lblDivisor.Size = new System.Drawing.Size(86, 13);
+            this.lblDivisor.TabIndex = 3;
+            this.lblDivisor.Text = "Ingrese el divisor";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(12, 128);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(143, 33);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(143, 91);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 9;
+            this.btnSalir.Text = "&Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(143, 62);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 8;
+            this.btnLimpiar.Text = "&Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(12, 144);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(206, 38);
+            this.txtResultado.TabIndex = 6;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 205);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblDivisor);
+            this.Controls.Add(this.lblDividendo);
+            this.Controls.Add(this.txtDivisor);
+            this.Controls.Add(this.txtDividendo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPrincipal";
+            this.Text = "Programa de Division";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDividendo;
+        private System.Windows.Forms.TextBox txtDivisor;
+        private System.Windows.Forms.Label lblDividendo;
+        private System.Windows.Forms.Label lblDivisor;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
+
---
+++ frmPrincipal.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejemploDivision
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void limpiar()
+        {
+            txtDividendo.Clear();
+            txtDivisor.Clear();
+            txtResultado.Clear();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            calcular();
+        }
+
+        public void calcular()
+        {
+            double dividendo, divisor, resultado = 0;
+
+            try
+            {
+                dividendo = Double.Parse(txtDividendo.Text);
+                divisor = Double.Parse(txtDivisor.Text);
+                resultado = Math.Round((dividendo / divisor),2);
+            }
+            catch(Exception x)
+            {
+                MessageBox.Show("Error: " + x.Message + " Ingrese valores númericos", "Validación",MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                txtResultado.Text = "Operación sin éxito. Cálculo inválido.";
+                txtDividendo.Focus();
+                return;
+            }
+
+            if (double.IsInfinity(resultado) || double.IsPositiveInfinity(resultado) || double.IsNegativeInfinity(resultado))
+            {
+                MessageBox.Show("Se está realizando una división entre cero","Validación",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                txtDivisor.Focus();
+            }
+            else
+            {
+                txtResultado.Text = resultado.ToString();
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+        }
+    }
+}
